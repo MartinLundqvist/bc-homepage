5c19ed1e026c2af29ddd58f19ff9b0e5f18b8e8b
MartinLundqvist <iphonelynden@gmail.com>
2021-10-06 14:30:49 +0200

    Clean-up

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Header from './components/Header';
 import NavigationLeft from './components/NavigationLeft';
 import NavigationRight from './components/NavigationRight';
 import Content from './components/Content/Content';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <React.Fragment>
       <Header />
