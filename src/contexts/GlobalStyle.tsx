49db426d2d613242b09454763297bf1ba5fd08cc
MartinLundqvist <iphonelynden@gmail.com>
2021-10-21 22:17:57 +0200

    Switched theme

--- src/contexts/GlobalStyle.tsx
+++ src/contexts/GlobalStyle.tsx
@@ -8,9 +8,7 @@ const GlobalStyle = createGlobalStyle<{ theme: ThemeType }>`
     display: flex;
     flex-direction: column;
     flex: 1;
-    font-family: '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Oxygen',
-      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
-      sans-serif;
+    font-family: 'Miriam Libre', sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     background-color: ${(props) => props.theme.colors.bg};
